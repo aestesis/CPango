9a7881121ccd69ed1af3f3b81035fbb7e99c9f57
renan <renan@aestesis.org>
2017-10-18 10:08:25 +0200

    simplification

--- Package.swift
+++ Package.swift
@@ -1,3 +1,14 @@
-import PackageDescription 
+// swift-tools-version:4.0
+import PackageDescription
 
-let package = Package(name: "CPango")
+let package = Package(
+    name: "CPango",
+    products: [
+        .library(name: "CPango",targets: ["CPango"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "CPango",dependencies: [])
+    ]
+)
