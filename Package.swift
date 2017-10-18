6c85a60a032da2f7a56b8a84db960449d2e0080a
renan <renan@aestesis.org>
2017-10-18 10:40:01 +0200

    added pkgconfig

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CPango",
-    pkgConfig: "Pango-1.0",
+    pkgConfig: "pango",
 	products: [
         .library(name: "CPango",targets: ["CPango"]),
     ],
