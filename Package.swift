52dcc8a52681fdccd1b69aa6e4d288ac31907e63
renan <renan@aestesis.org>
2017-10-19 12:02:58 +0200

    pkgconfig

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "CPango",
-    pkgConfig: "pango",
+    pkgConfig: "pango freetype2",
     providers: [ 
-    	.apt(["libpango1.0-dev freetype"]) 
+    	.apt(["libpango1.0-dev freetype2"]) 
     ],
 	products: [
         .library(name: "CPango",targets: ["CPango"])
