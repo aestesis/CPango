2a3a7c9b176afefd1880522d8f57f85419bffe95
renan <renan@aestesis.org>
2017-10-19 11:48:00 +0200

    fixed umbrella

--- umbrela.h
+++ umbrela.h
@@ -1,3 +1,3 @@
-#include "/usr/include/pango-10/pango/pango.h"
-#include "/usr/include/pango-10/pango/pangoft2.h"
+#include "/usr/include/pango-1.0/pango/pango.h"
+#include "/usr/include/pango-1.0/pango/pangoft2.h"
 #include "/usr/include/freetype2/ft2build.h"
