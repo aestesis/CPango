aa69323bd1e9327bc8ce3ef58c61731a33a494ce
renan <renan@aestesis.org>
2019-02-22 06:44:18 +0100

    up

--- Package.swift
+++ Package.swift
@@ -1,11 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
-import PackageDescription 
+import PackageDescription
 
-let package = Package(name: "CPango",
-	pkgConfig: "pango",
-	providers: [
-		.brew(["pango", "glib", "glib-networking", "gobject-introspection"]),
-		.apt(["libpango1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
-	]
+let package = Package(
+    name: "CPango",
+	products: [
+        .library(name: "CPango",targets: ["CPango"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .systemLibrary(name: "CPango")
+    ]
 )
